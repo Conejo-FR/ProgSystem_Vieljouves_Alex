3b632dfdace941c19b05f54a5c61e8e3d17e387c
ALEX VIELJOUVES <alex.vieljouves@iut.local>
2026-09-08 16:20:41 +0200

    création de la première image

--- Image.java
+++ Image.java
@@ -4,28 +4,31 @@ import java.io.IOException;
 public class Image {
     private int width;
     private int height;
-    private int hauteur;
-    private int largeur;
-        // pixels[y][x][0=R,1=G,2=B]
-    private int[][][] pixels; // pixels[y][x][0=R,1=G,2=B]
+    private int[][][] pixels;   // pixels[y][x][0=R,1=G,2=B]
 
     public int getWidth() { return width; }
     public int getHeight() { return height; }
 
+    public static void main(String[] args){
+        Image Image_pixels = new Image(100,100);
+        setPixel(10,10,0,1,1)
+
+    }
+
     /**
      * Constructeur : initialise une image vide.
      */
-    public Image(int width, int hauteur) {
+    public Image(int width, int height) {
         this.width = width;
         this.height = height;
-        pixels = new int[hauteur][largeur][3];
+        pixels = new int[width][height][3];
     }
 
     /**
      * Définit la couleur d'un pixel à la position (x, y)
      */
     public void setPixel(int x, int y, int r, int g, int b) {
-        if (x >= 0 && x < largeur && y >= 0 && y < hauteur) {
+        if (x >= 0 && x < height && y >= 0 && y < width) {
             pixels[y][x][0] = r;
             pixels[y][x][1] = g;
             pixels[y][x][2] = b;
@@ -36,6 +39,14 @@ public class Image {
      * Sauvegarde l'image au format texte PPM (P3)
      */
     public void save_txt(String filename) throws IOException {
-        // TODO : écrire le fichier PPM avec FileWriter
+        try {
+            FileWriter writer = new FileWriter(filename);
+
+            
+
+            System.out.println("Image PPM créée avec succès !");
+        } catch (IOException e) {
+            System.err.println("Erreur lors de l'écriture du fichier : " + e.getMessage());
+        }
     }
 }
